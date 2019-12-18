9ad30cd1e288d8536a8597b5de339a8a1401650e
Jérémie Vincke <jeremie@movify.com>
2019-12-18 12:00:32 +0100

    Added Qualtrics dependency

--- example/android/app/src/main/kotlin/be/vrt/qualtrics_flutter_example/MainActivity.kt
+++ example/android/app/src/main/kotlin/be/vrt/qualtrics_flutter_example/MainActivity.kt
@@ -1,12 +1,23 @@
 package be.vrt.qualtrics_flutter_example
 
-import androidx.annotation.NonNull;
+import android.os.Bundle
+import androidx.annotation.NonNull
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugins.GeneratedPluginRegistrant
+import com.qualtrics.digital.*
 
-class MainActivity: FlutterActivity() {
+class MainActivity : FlutterActivity() {
+
     override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
-        GeneratedPluginRegistrant.registerWith(flutterEngine);
+        GeneratedPluginRegistrant.registerWith(flutterEngine)
     }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //TODO: test
+        Qualtrics.instance().initialize("BRAND ID", "PROJECT ID", "INTERCEPT ID", this)
+        //TODO: test
+    }
+
 }
