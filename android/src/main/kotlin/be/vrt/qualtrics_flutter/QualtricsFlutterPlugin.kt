9319de5f417632442f2b00ccf8035f8fbbbeedad
Jérémie Vincke <jeremie@movify.com>
2019-12-18 14:17:56 +0100

    Android implementation : added Extractor, updated QualtricsFlutterPlugin.kt & sample app manifest with Qualtrics activities

--- android/src/main/kotlin/be/vrt/qualtrics_flutter/QualtricsFlutterPlugin.kt
+++ android/src/main/kotlin/be/vrt/qualtrics_flutter/QualtricsFlutterPlugin.kt
@@ -1,6 +1,8 @@
 package be.vrt.qualtrics_flutter
 
-import androidx.annotation.NonNull;
+import android.content.Context
+import androidx.annotation.NonNull
+import com.qualtrics.digital.Qualtrics
 import io.flutter.embedding.engine.plugins.FlutterPlugin
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
@@ -9,37 +11,94 @@ import io.flutter.plugin.common.MethodChannel.Result
 import io.flutter.plugin.common.PluginRegistry.Registrar
 
 /** QualtricsFlutterPlugin */
-public class QualtricsFlutterPlugin: FlutterPlugin, MethodCallHandler {
-  override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
-    val channel = MethodChannel(flutterPluginBinding.getFlutterEngine().getDartExecutor(), "qualtrics_flutter")
-    channel.setMethodCallHandler(QualtricsFlutterPlugin());
-  }
+public class QualtricsFlutterPlugin : FlutterPlugin, MethodCallHandler {
 
-  // This static function is optional and equivalent to onAttachedToEngine. It supports the old
-  // pre-Flutter-1.12 Android projects. You are encouraged to continue supporting
-  // plugin registration via this function while apps migrate to use the new Android APIs
-  // post-flutter-1.12 via https://flutter.dev/go/android-project-migration.
-  //
-  // It is encouraged to share logic between onAttachedToEngine and registerWith to keep
-  // them functionally equivalent. Only one of onAttachedToEngine or registerWith will be called
-  // depending on the user's project. onAttachedToEngine or registerWith must both be defined
-  // in the same class.
-  companion object {
-    @JvmStatic
-    fun registerWith(registrar: Registrar) {
-      val channel = MethodChannel(registrar.messenger(), "qualtrics_flutter")
-      channel.setMethodCallHandler(QualtricsFlutterPlugin())
+    private var context: Context? = null
+
+    override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
+        val channel = MethodChannel(flutterPluginBinding.getFlutterEngine().getDartExecutor(), "qualtrics_flutter")
+        val plugin = QualtricsFlutterPlugin()
+        plugin.context = flutterPluginBinding.applicationContext
+        channel.setMethodCallHandler(plugin)
     }
-  }
 
-  override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
-    if (call.method == "getPlatformVersion") {
-      result.success("Android ${android.os.Build.VERSION.RELEASE}")
-    } else {
-      result.notImplemented()
+    // This static function is optional and equivalent to onAttachedToEngine. It supports the old
+    // pre-Flutter-1.12 Android projects. You are encouraged to continue supporting
+    // plugin registration via this function while apps migrate to use the new Android APIs
+    // post-flutter-1.12 via https://flutter.dev/go/android-project-migration.
+    //
+    // It is encouraged to share logic between onAttachedToEngine and registerWith to keep
+    // them functionally equivalent. Only one of onAttachedToEngine or registerWith will be called
+    // depending on the user's project. onAttachedToEngine or registerWith must both be defined
+    // in the same class.
+    companion object {
+        @JvmStatic
+        fun registerWith(registrar: Registrar) {
+            val channel = MethodChannel(registrar.messenger(), "qualtrics_flutter")
+            val plugin = QualtricsFlutterPlugin()
+            plugin.context = registrar.context()
+            channel.setMethodCallHandler(plugin)
+        }
     }
-  }
 
-  override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
-  }
+    override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
+        when (val data = Extractor.qualtricsCallFromCall(call)) {
+            is Extractor.QualtricsCall.Init -> init(data, result)
+            is Extractor.QualtricsCall.EvaluateTargetingLogic -> evaluateTargetingLogic(result)
+            is Extractor.QualtricsCall.Display -> display(result)
+            is Extractor.QualtricsCall.DisplayTarget -> displayTarget(data, result)
+            is Extractor.QualtricsCall.Hide -> hide(result)
+            is Extractor.QualtricsCall.RegisterViewVisit -> registerViewVisit(data, result)
+            is Extractor.QualtricsCall.ResetTimer -> resetTimer(result)
+            is Extractor.QualtricsCall.ResetViewCounter -> resetViewCounter(result)
+            Extractor.QualtricsCall.Unknown -> result.notImplemented()
+        }.exhaustive
+    }
+
+    private fun init(parameters: Extractor.QualtricsCall.Init, result: Result) {
+        Qualtrics.instance().initialize(parameters.brandId, parameters.zoneId, parameters.interceptId, context)
+        result.success(true)
+    }
+
+    private fun evaluateTargetingLogic(result: Result) {
+        Qualtrics.instance().evaluateTargetingLogic {
+            //TODO: handke targetingResult here
+            result.success(true)
+        }
+    }
+
+    private fun display(result: Result) {
+        result.success(Qualtrics.instance().display(context))
+    }
+
+    private fun displayTarget(parameter: Extractor.QualtricsCall.DisplayTarget, result: Result) {
+        Qualtrics.instance().displayTarget(context, parameter.target)
+        result.success(true)
+    }
+
+    private fun hide(result: Result) {
+        result.success(Qualtrics.instance().hide())
+    }
+
+    private fun registerViewVisit(parameter: Extractor.QualtricsCall.RegisterViewVisit, result: Result) {
+        Qualtrics.instance().registerViewVisit(parameter.viewName)
+        result.success(true)
+    }
+
+    private fun resetTimer(result: Result) {
+        Qualtrics.instance().resetTimer()
+        result.success(true)
+    }
+
+    private fun resetViewCounter(result: Result) {
+        Qualtrics.instance().resetViewCounter()
+        result.success(true)
+    }
+
+    override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
+    }
+
 }
+
+private val <T> T.exhaustive: T
+    get() = this
